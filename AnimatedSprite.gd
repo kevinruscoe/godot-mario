e9401b6ff90d1fb2225baca1a0f84bae43134182
Kevin Ruscoe <kevdotbadger@gmail.com>
2022-06-25 13:23:21 +0100

    Fix stripes

--- AnimatedSprite.gd
+++ AnimatedSprite.gd
@@ -1,8 +1,8 @@
 extends AnimatedSprite
 
 var flipped = false
-
 var is_jumping = false
+var is_moving= false
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -13,10 +13,14 @@ func _process(delta):
 	flip_h = flipped
 
 func _on_KinematicBody2D_has_become_stationary():
+	is_moving = false
+	
 	if not is_jumping:
 		play("idle")
 
 func _on_KinematicBody2D_has_started_moving():
+	is_moving = true
+	
 	if not is_jumping:
 		play("walking")
 
@@ -26,3 +30,7 @@ func _on_KinematicBody2D_has_jumped():
 
 func _on_KinematicBody2D_has_landed():
 	is_jumping = false
+	if is_moving:
+		play("walking")
+	else:
+		play("idle")
--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -7,6 +7,7 @@ export (int) var gravity = 300
 var velocity = Vector2.ZERO
 
 var is_jumping = false
+var is_moving = false
 
 export (float, 0, 1.0) var friction = 0.18
 export (float, 0, 1.0) var acceleration = 0.25
@@ -15,35 +16,68 @@ signal has_become_stationary
 signal has_started_moving
 signal has_jumped
 signal is_falling
+signal is_rising
+signal is_moving
 signal has_landed
 
-func _physics_process(delta):
+func _ready():
+	connect("has_jumped", self, "_on_jump")
+	connect("has_landed", self, "_on_landed")
+	connect("has_become_stationary", self, "_on_has_become_stationary")
+	connect("has_started_moving", self, "_on_has_started_moving")
+	connect("is_falling", self, "_on_is_falling")
+	connect("is_rising", self, "_on_is_rising")
+
+func _physics_process(delta):	
+	var direction = Vector2.ZERO
 	
-	var dir = 0
 	if Input.is_action_pressed("ui_right"):
-		dir += 1
+		direction.x = 1
 	if Input.is_action_pressed("ui_left"):
-		dir -= 1
+		direction.x = -1
 		
-	if dir != 0:
-		emit_signal("has_started_moving")
-		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	if direction.x != 0:
+		if not is_moving:
+			emit_signal("has_started_moving")
+		velocity.x = lerp(velocity.x, direction.x * speed, acceleration)
 	else:
-		emit_signal("has_become_stationary")
+		if is_moving:
+			emit_signal("has_become_stationary")
 		velocity.x = lerp(velocity.x, 0, friction)
 	
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	if Input.is_action_just_pressed("ui_jump"):
+		if is_on_floor():
+			emit_signal("has_jumped")
+	
 	if velocity.y < 0:
 		emit_signal("is_falling")
-	
-	if Input.is_action_just_pressed("ui_select"):
-		if is_on_floor():
-			is_jumping = true
-			emit_signal("has_jumped")
-			velocity.y = jump_speed
+	elif velocity.y > 0:
+		emit_signal("is_rising")
+		
+	if not velocity.x == 0:
+		emit_signal("is_moving")
 			
 	if is_jumping and velocity.y == 0:
 		emit_signal("has_landed")
-		is_jumping = false
+
+func _on_jump():
+	is_jumping = true
+	velocity.y = jump_speed
+	
+func _on_landed():
+	is_jumping = false
+
+func _on_has_become_stationary():
+	is_moving = false
+
+func _on_has_started_moving():
+	is_moving = true
+
+func _on_is_falling():
+	pass
+	
+func _on_is_rising():
+	pass
